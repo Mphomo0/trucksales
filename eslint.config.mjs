52b31951cf51ad7279e5067bdc88950799810b5e
Mphomo0 <mphomoipolai1@gmail.com>
2025-12-07 08:43:41 +0200

    Revert "upgraded to nextjs16"
    This reverts commit 6f6588f7466b5d5e4b9c6c9d7e7eb6670a49bf24.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-import nextTypescript from "eslint-config-next/typescript";
-import next from "eslint-config-next";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -12,10 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...nextTypescript, ...next, ...nextCoreWebVitals, ...compat.config({
-  extends: ['prettier']
-}), {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}]
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next', 'next/core-web-vitals', 'prettier'],
+  }),
+]
 
 export default eslintConfig
