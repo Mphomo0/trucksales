732ab189425751cfcb58a0e7351aaf5ad56272ad
Mphomo0 <mphomoipolai1@gmail.com>
2026-05-16 14:30:47 +0200

    fix Bing verification using verification.bing property

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,13 +89,8 @@ export const metadata: Metadata = {
   },
   verification: {
     google: 'NSttRQu748qaKY6XHYe8AVd8vlsBpvXA_q8EcB7BI_Q',
+    bing: '6F4E7F1FC1EFA944472DB933122CB39F',
   },
-  meta: [
-    {
-      name: 'msvalidate.01',
-      content: '6F4E7F1FC1EFA944472DB933122CB39F',
-    },
-  ],
 }
 
 const organizationSchema = {
